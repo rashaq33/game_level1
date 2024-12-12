a771fb86ca83b85ce6ea9759610333a882c54e8c
rashaq3390 <rashaq3390@gmail.com>
2024-12-12 15:20:06 +0300

    Initial Commit

---
+++ game_level1Tests/game_level1Tests.swift
@@ -0,0 +1,17 @@
+//
+//  game_level1Tests.swift
+//  game_level1Tests
+//
+//  Created by رشا القرني on 11/06/1446 AH.
+//
+
+import Testing
+@testable import game_level1
+
+struct game_level1Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
